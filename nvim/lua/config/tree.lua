b4022a3ac6a6aadd1eeb58258b65942ba6933e6f
Tres Sims <tres.sims@dash-orlando.com>
2022-04-28 10:32:32 -0400

    nvim: removed invalid configuration sets for nvim-tree

--- nvim/lua/config/tree.lua
+++ nvim/lua/config/tree.lua
@@ -3,7 +3,6 @@ require'nvim-tree'.setup {
   hijack_netrw         = true,
   open_on_setup        = false,
   ignore_ft_on_setup   = {},
-  auto_close           = false,
   auto_reload_on_write = true,
   open_on_tab          = false,
   hijack_cursor        = false,
@@ -45,7 +44,6 @@ require'nvim-tree'.setup {
     height = 30,
     hide_root_folder = false,
     side = 'left',
-    auto_resize = false,
     preserve_window_proportions = false,
     mappings = {
       custom_only = false,
@@ -65,10 +63,10 @@ require'nvim-tree'.setup {
       global = false,
     },
     open_file = {
-      quit_on_open = false,
+      quit_on_open = true,
       window_picker = {
         enable = true,
-        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+        chars = "asdfjkl",
         exclude = {
           filetype = {
             "notify",
