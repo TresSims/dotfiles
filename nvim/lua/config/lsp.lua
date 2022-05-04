2e8ce5bdfa917486cd446e411cc232315201b4eb
Tres Sims <tres.sims@dash-orlando.com>
2022-05-04 08:48:05 -0400

    nvim: changed diagnostics from inline to popup

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -10,9 +10,20 @@ require'lspconfig'.lemminx.setup{
         }
 }
 
+-- cpp LSP
+require'lspconfig'.clangd.setup{}
+
 vim.cmd([[ 
         augroup coq-start
                 autocmd!
                 au bufEnter * :COQnow --shut-up
         augroup END
 ]])
+
+vim.diagnostic.config({
+  virtual_text = false
+})
+
+-- Show line diagnostics automatically in hover window
+vim.o.updatetime = 250
+vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
