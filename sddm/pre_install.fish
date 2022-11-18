02e4a42cb438bcc5d6d24969202ed67386e58915
Tres Sims <tres.sims@dash-orlando.com>
2022-11-18 09:41:54 -0500

    Sddm: added proper flags to install script

--- sddm/pre_install.fish
+++ sddm/pre_install.fish
@@ -1,7 +1,7 @@
 #! /usr/bin/fish
 
 sudo rm -rf /usr/share/sddm/themes/multicolor-sddm-theme
-sudo cp $PUNCTUAL_PACKAGE_DIR/sddm/multicolor-sddm-theme /usr/share/sddm/themes/
+sudo cp -r $PUNCTUAL_PACKAGE_DIR/sddm/multicolor-sddm-theme /usr/share/sddm/themes/
 
 sudo rm /usr/lib/sddm/sddm.conf.d/default.conf
 sudo ln -s $PUNCTUAL_PACKAGE_DIR/sddm/default.conf /usr/lib/sddm/sddm.conf.d/
