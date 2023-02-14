835321f7cd686a55f982a4306ead68e7603c2afb
Tres Sims <tres.sims95@gmail.com>
2023-02-13 21:49:05 -0500

    nvim: updated lua lsp

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -10,16 +10,12 @@ require("lspconfig").jsonls.setup({
 	capabilities = capabilities,
 })
 
-require("lspconfig").sumneko_lua.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
+require("lspconfig").lua_ls.setup({
 	settings = {
 		Lua = {
 			runtime = {
 				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
 				version = "LuaJIT",
-				-- Setup your lua path
-				path = runtime_path,
 			},
 			diagnostics = {
 				-- Get the language server to recognize the `vim` global
