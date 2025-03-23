50bf01af223c21760433071cefb8410aa07d2c16
Tres Sims <tres.sims95@gmail.com>
2025-03-22 22:49:38 -0500

    Update wacom target

--- fish/functions/wacom.fish
+++ fish/functions/wacom.fish
@@ -1,5 +1,6 @@
 function wacom
-    xsetwacom set 12 MapToOutput NEXT
-    xsetwacom set 13 MapToOutput NEXT
-    xsetwacom set 14 MapToOutput NEXT
+    xsetwacom set 8 MapToOutput NEXT
+    xsetwacom set 9 MapToOutput NEXT
+    xsetwacom set 10 MapToOutput NEXT
+    xsetwacom set 11 MapToOutput NEXT
 end
