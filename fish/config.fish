cf95c1306af1e93bdc73fdf867097f70b1786ef4
Tres Sims <tres.sims95@gmail.com>
2025-05-06 19:34:59 -0500

    Fish updates for cleaner completions

--- fish/config.fish
+++ fish/config.fish
@@ -35,6 +35,9 @@ set -g fish_pager_color_prefix $cyan
 set -g fish_pager_color_completion $foreground
 set -g fish_pager_color_description $comment
 
-source /opt/asdf-vm/asdf.fish
+# Source completion dir
+for file in (ls $PUNCTUAL_PACKAGE_DIR/fish/completions/)
+    source $PUNCTUAL_PACKAGE_DIR/fish/completions/$file
+end
 
 starship init fish | source
