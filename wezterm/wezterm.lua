5ecf9537c8d7316b8dcba63ff22bc2a9fb928d16
Tres Sims <tres.sims95@gmail.com>
2025-05-07 23:18:43 -0500

    The terminal update

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require("wezterm")
+
+return {
+	hide_tab_bar_if_only_one_tab = true,
+	font_size = 14.0,
+	font = wezterm.font("FiraCode Nerd Font"),
+	color_scheme = "Tokyo Night Moon",
+}
