e2fa3966460bea3175d323962c89bca09fae7e37
Tres Sims <tres.sims95@gmail.com>
2025-11-15 15:47:35 -0600

    Fix fish variable writing

--- dot_config/fish/functions/save-vars.fish
+++ dot_config/fish/functions/save-vars.fish
@@ -1,3 +1,3 @@
 function save-vars --wraps=vault\ kv\ put\ -mount=chezmoi\ laptop-01.tres.sims.family/fish_variables\ data=
-    vault kv put -mount=chezmoi laptop-01.tres.sims.family/fish_variables data=(cat ~/.config/fish/fish_variables)
+    vault kv put -mount=chezmoi (hostname)/fish_variables data=(cat ~/.config/fish/fish_variables)
 end
