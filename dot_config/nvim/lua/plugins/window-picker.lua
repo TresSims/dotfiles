2c02866382948672df345a5cb411324ce6827ed9
Tres Sims <tres.sims95@gmail.com>
2025-12-16 21:11:38 -0600

    Changesss

--- dot_config/nvim/lua/plugins/window-picker.lua
+++ dot_config/nvim/lua/plugins/window-picker.lua
@@ -1,25 +1,15 @@
 -- add window picker
 
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    window = {
-      mappings = {
-        ["<cr>"] = "open_with_window_picker",
-      },
-    },
-  },
-  dependencies = {
-    {
-      "s1n7ax/nvim-window-picker",
-      name = "window-picker",
-      event = "VeryLazy",
-      config = function()
-        require("window-picker").setup()
-      end,
-      opts = {
-        selection_chars = "FJDKSLA;",
-      },
-    },
-  },
+	{
+		"s1n7ax/nvim-window-picker",
+		name = "window-picker",
+		event = "VeryLazy",
+		config = function()
+			require("window-picker").setup()
+		end,
+		opts = {
+			selection_chars = "FJDKSLA;",
+		},
+	},
 }
