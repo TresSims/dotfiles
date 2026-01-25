18f713b76e3b25c9b9160cf430944db2f333da27
Tres Sims <tres.sims95@example.com>
2026-01-24 20:28:02 -0600

    Treesitter updates

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -21,5 +21,43 @@ return {
 				"go",
 			},
 		},
+		config = function(_, opts)
+			local TS = require("nvim-treesitter")
+
+			-- setup treesitter
+			TS.setup(opts)
+
+			vim.api.nvim_create_autocmd("FileType", {
+				group = vim.api.nvim_create_augroup("lazyvim_treesitter", { clear = true }),
+				callback = function(ev)
+					local ft, lang = ev.match, vim.treesitter.language.get_lang(ev.match)
+					if not Util.treesitter.have(ft) then
+						return
+					end
+
+					local function enabled(feat, query)
+						local f = opts[feat] or {}
+						return f.enable ~= false
+							and not (type(f.disable) == "table" and vim.tbl_contains(f.disable, lang))
+							and Util.treesitter.have(ft, query)
+					end
+
+					-- highlighting
+					if enabled("highlight", "highlights") then
+						pcall(vim.treesitter.start, ev.buf)
+					end
+
+					-- indents
+					if enabled("indent", "indents") then
+						vim.opt.indentexpr = Util.treesitter.have(nil, "indents") and TS.indentexpr() or -1
+					end
+
+					-- folds
+					if enabled("folds", "folds") then
+						vim.opt.foldexpr = Util.treesitter.have(nil, "folds") and vim.treesitter.foldexpr() or "0"
+					end
+				end,
+			})
+		end,
 	},
 }
--- dot_config/nvim/lua/util/init.lua
+++ dot_config/nvim/lua/util/init.lua
@@ -4,4 +4,6 @@ M.root = require("util.root")
 
 M.ui = require("util.ui")
 
+M.treesitter = require("util.treesitter")
+
 return M
