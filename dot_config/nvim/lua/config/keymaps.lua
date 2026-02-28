75f0d65e32bb7f93d39543f37cb10e64abfae18b
Tres Sims <tres.sims95@gmail.com>
2026-02-28 13:59:06 -0600

    add new tabpage hotkey

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -54,4 +54,5 @@ wk.add({
 	{ "<leader><tab>", "Tab Pages" },
 	{ "<leader><tab>j", ":tabn<CR>", desc = "Go to next tabpage" },
 	{ "<leader><tab>k", ":tabp<CR>", desc = "Go to previous tabpage" },
+	{ "<leader><tab><tab>", ":tabnew<CR>", desc = "Create new tabpage" },
 })
